97e34c8d9e36d463a696ff911c39303ee756ba60
fumiaki <haratairani3000points@gmail.com>
2016-03-15 11:41:24 +0900

    初期設定 config.php bootstrap.php 修正

--- fuelphp/fuel/fuel/app/config/config.php
+++ fuelphp/fuel/fuel/app/config/config.php
@@ -78,8 +78,10 @@ return array(
 	 * Localization & internationalization settings
 	 */
 	// 'language'           => 'en', // Default language
+	'language'           => 'ja', // Default language
 	// 'language_fallback'  => 'en', // Fallback language when file isn't available for default language
 	// 'locale'             => 'en_US', // PHP set_locale() setting, null to not set
+	'locale'             => null,
 
 	/**
 	 * Internal string encoding charset
@@ -94,6 +96,8 @@ return array(
 	 */
 	// 'server_gmt_offset'  => 0,
 	// 'default_timezone'   => null,
+	'default_timezone' => 'Asia/Tokyo',
+
 
 	/**
 	 * Logging Threshold.  Can be set to any of the following:
@@ -106,6 +110,7 @@ return array(
 	 * Fuel::L_ALL
 	 */
 	// 'log_threshold'    => Fuel::L_WARNING,
+	'log_threshold'    => Fuel::L_ALL,
 	// 'log_path'         => APPPATH.'logs/',
 	// 'log_date_format'  => 'Y-m-d H:i:s',
 
@@ -121,6 +126,7 @@ return array(
 		 * A salt to make sure the generated security tokens are not predictable
 		 */
 		// 'token_salt'            => 'put your salt value here to make the token more secure',
+		'token_salt'            => 'fuelphpnotesutodesu',
 
 		/**
 		 * Allow the Input class to use X headers when present
